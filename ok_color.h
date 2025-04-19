b6e1e103d99450388dd5c1da23f44b9fda40bb8d
Dan Egnor <egnor@ofb.net>
2025-04-19 04:46:08 -0700

    initial checkin

---
+++ ok_color.h
@@ -0,0 +1,8 @@
+// Embedded-friendly representation of colors in useful color spaces.
+
+#pragma once
+
+// 8-bit sRGB colors, as used in PC monitors, #xxxxxx codes, etc.
+TODO - add note about gamma
+struct ok_standard_rgb {
+};
