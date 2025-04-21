4530c2de30666955f21a18f60181fb34a185e2d0
Dan Egnor <egnor@ofb.net>
2025-04-21 04:24:45 +0200

    more silliness

--- src/ok_color_rgb.h
+++ src/ok_color_rgb.h
@@ -36,90 +36,87 @@ struct ok_hsv32 { uint16_t h; uint8_t s, v; };
 // 48-bit HSV. All components range 0-65535; hue 0=red, 21845=green, 43690=blue.
 struct ok_hsv48 { uint16_t h, s, v; };
 
+//
 // Color space conversions
+//
 
-#define CHAIN_FROM(ta, tb, tc) \
-  inline ta ta##_from(tc c) { return ta##_from(tb##_from(c)); }
+// Simple ratio RGB conversions (G ratio specified separately to allow 5:6:5)
+#define G_RB_RATIO_BETWEEN(A, B, j, k, a, b) \
+  inline A A##_from(B x) { return { x.r * a / b, x.g * j / k, x.b * a / b }; } \
+  inline B B##_from(A x) { return { x.r * b / a, x.g * k / j, x.b * b / a }; }
 
-ok_srgb24 ok_srgb24_from(ok_srgb16);
-ok_srgb24 ok_srgb24_from(ok_srgb48);
-ok_srgb24 ok_srgb24_from(ok_lrgb24);
-ok_srgb24_ok_srgb24_from(ok_lrgb48);
-ok_srgb24 ok_srgb24_from(ok_hsv24);
-ok_srgb24 ok_srgb24_from(ok_hsv360);
-ok_srgb24 ok_srgb24_from(ok_hsv32);
-ok_srgb24 ok_srgb24_from(ok_hsv48);
+// Simple ratio HSV conversions (H ratio specified separately)
+#define H_SV_RATIO_BETWEEN(A, B, j, k, a, b) \
+  inline A A##_from(B x) { return { x.h * j / k, x.s * a / b, x.v * a / b }; } \
+  inline B B##_from(A x) { return { x.h * j / k, x.s * b / a, x.v * b / a }; }
 
-ok_srgb16 ok_srgb16_from(ok_srgb24);
-ok_srgb16 ok_srgb16_from(ok_srgb48);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_lrgb24);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_lrgb48);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_hsv24);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_hsv360);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_hsv32);
-CHAIN_FROM(ok_srgb16, ok_srgb24, ok_hsv48);
+// Multi-step conversions
+#define CHAIN_BETWEEN(A, B, C) \
+  inline A A##_from(C x) { return A##_from(B##_from(x)); } \
+  inline C C##_from(A x) { return C##_from(B##_from(x)); } \
 
-ok_srgb48 ok_srgb48_from(ok_srgb24);
-ok_srgb48 ok_srgb48_from(ok_srgb16);
-ok_srgb48 ok_srgb48_from(ok_lrgb24);
-ok_srgb48 ok_srgb48_from(ok_lrgb48);
-CHAIN_FROM(ok_srgb48, ok_srgb24, ok_hsv24);
-CHAIN_FROM(ok_srgb48, ok_srgb24, ok_hsv360);
-ok_srgb48 ok_srgb48_from(ok_hsv32);
-ok_srgb48 ok_srgb48_from(ok_hsv48);
+G_RB_RATIO_BETWEEN(ok_srgb16, ok_srgb24, 63, 255, 31, 255);
+G_RB_RATIO_BETWEEN(ok_srgb16, ok_srgb48, 63, 65535, 31, 65535);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb24, ok_lrgb24);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb24, ok_lrgb48);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb24, ok_hsv24);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb24, ok_hsv360);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb48, ok_hsv32);
+CHAIN_BETWEEN(ok_srgb16, ok_srgb48, ok_hsv48);
 
-ok_lrgb24 ok_lrgb24_from(ok_srgb24);
-CHAIN_FROM(ok_lrgb24, ok_srgb24, ok_srgb16);
-ok_lrgb24 ok_lrgb24_from(ok_srgb48);
-ok_lrgb24 ok_lrgb24_from(ok_lrgb48);
-CHAIN_FROM(ok_lrgb24, ok_srgb24, ok_hsv24);
-CHAIN_FROM(ok_lrgb24, ok_srgb24, ok_hsv360);
-CHAIN_FROM(ok_lrgb24, ok_srgb48, ok_hsv32);
-CHAIN_FROM(ok_lrgb24, ok_srgb48, ok_hsv48);
+G_RB_RATIO_BETWEEN(ok_srgb24, ok_srgb48, 255, 65535, 255, 65535);
+CHAIN_BETWEEN(ok_srgb24, ok_srgb48, ok_lrgb24);
+CHAIN_BETWEEN(ok_srgb24, ok_srgb48, ok_lrgb48);
+CHAIN_BETWEEN(ok_srgb24, ok_hsv32, ok_hsv24);
+CHAIN_BETWEEN(ok_srgb24, ok_hsv32, ok_hsv360);
+// ok_srgb24 <-> ok_hsv32 defined below
+CHAIN_BETWEEN(ok_srgb24, ok_hsv32, ok_hsv48);
 
-ok_lrgb48 ok_lrgb48_from(ok_srgb24);
-CHAIN_FROM(ok_lrgb48, ok_srgb24, ok_srgb16);
-ok_lrgb48 ok_lrgb48_from(ok_srgb48);
-ok_lrgb48 ok_lrgb48_from(ok_lrgb24);
-CHAIN_FROM(ok_lrgb48, ok_srgb24, ok_hsv24);
-CHAIN_FROM(ok_lrgb48, ok_srgb24, ok_hsv360);
-CHAIN_FROM(ok_lrgb48, ok_srgb48, ok_hsv32);
-CHAIN_FROM(ok_lrgb48, ok_srgb48, ok_hsv48);
+CHAIN_BETWEEN(ok_srgb48, ok_lrgb48, ok_lrgb24);
+// ok_srgb48 <-> ok_lrgb48 defined below
+CHAIN_BETWEEN(ok_srgb48, ok_srgb24, ok_hsv24);
+CHAIN_BETWEEN(ok_srgb48, ok_srgb24, ok_hsv360);
+CHAIN_BETWEEN(ok_srgb48, ok_hsv48, ok_hsv32);
+// ok_srgb48 <-> ok_hsv48 defined below
 
-ok_hsv24 ok_hsv24_from(ok_srgb24);
-CHAIN_FROM(ok_hsv24, ok_srgb24, ok_srgb16);
-CHAIN_FROM(ok_hsv24, ok_srgb24, ok_srgb48);
-CHAIN_FROM(ok_hsv24, ok_srgb24, ok_lrgb24);
-CHAIN_FROM(ok_hsv24, ok_srgb24, ok_lrgb48);
-ok_hsv24 ok_hsv24_from(ok_hsv360);
-ok_hsv24 ok_hsv24_from(ok_hsv32);
-ok_hsv24 ok_hsv24_from(ok_hsv48);
+G_RB_RATIO_BETWEEN(ok_lrgb24, ok_lrgb48, 255, 65535, 255, 65535);
+CHAIN_BETWEEN(ok_lrgb24, ok_srgb24, ok_hsv24);
+CHAIN_BETWEEN(ok_lrgb24, ok_srgb24, ok_hsv360);
+CHAIN_BETWEEN(ok_lrgb24, ok_srgb48, ok_hsv32);
+CHAIN_BETWEEN(ok_lrgb24, ok_srgb48, ok_hsv48);
 
-ok_hsv360 ok_hsv360_from(ok_srgb24);
-CHAIN_FROM(ok_hsv360, ok_srgb24, ok_srgb16);
-CHAIN_FROM(ok_hsv360, ok_srgb24, ok_srgb48);
-CHAIN_FROM(ok_hsv360, ok_srgb24, ok_lrgb24);
-CHAIN_FROM(ok_hsv360, ok_srgb24, ok_lrgb48);
-ok_hsv360 ok_hsv360_from(ok_hsv24);
-ok_hsv360 ok_hsv360_from(ok_hsv32);
-ok_hsv360 ok_hsv360_from(ok_hsv48);
+CHAIN_BETWEEN(ok_lrgb48, ok_srgb24, ok_hsv24);
+CHAIN_BETWEEN(ok_lrgb48, ok_srgb24, ok_hsv360);
+CHAIN_BETWEEN(ok_lrgb48, ok_srgb48, ok_hsv32);
+CHAIN_BETWEEN(ok_lrgb48, ok_srgb48, ok_hsv48);
 
-ok_hsv32 ok_hsv32_from(ok_srgb24);
-CHAIN_FROM(ok_hsv32, ok_srgb24, ok_srgb16);
-ok_hsv32 ok_hsv32_from(ok_srgb48);
-CHAIN_FROM(ok_hsv32, ok_srgb48, ok_lrgb24);
-CHAIN_FROM(ok_hsv32, ok_srgb48, ok_lrgb48);
-ok_hsv32 ok_hsv32_from(ok_hsv24);
-ok_hsv32 ok_hsv32_from(ok_hsv360);
-ok_hsv32 ok_hsv32_from(ok_hsv48);
+H_SV_RATIO_BETWEEN(ok_hsv24, ok_hsv360, 255, 360, 255, 100);
+H_SV_RATIO_BETWEEN(ok_hsv24, ok_hsv32, 255, 65535, 255, 255);
+H_SV_RATIO_BETWEEN(ok_hsv24, ok_hsv48, 255, 65535, 255, 65535);
 
-ok_hsv48 ok_hsv48_from(ok_srgb24);
-CHAIN_FROM(ok_hsv48, ok_srgb24, ok_srgb16);
-ok_hsv48 ok_hsv48_from(ok_srgb48);
-CHAIN_FROM(ok_hsv48, ok_srgb48, ok_lrgb24);
-CHAIN_FROM(ok_hsv48, ok_srgb48, ok_lrgb48);
-CHAIN_FROM(ok_hsv48, ok_hsv32, ok_hsv24);
-ok_hsv48 ok_hsv48_from(ok_hsv360);
-ok_hsv48 ok_hsv48_from(ok_hsv32);
+H_SV_RATIO_BETWEEN(ok_hsv360, ok_hsv32, 360, 65535, 255, 255);
+H_SV_RATIO_BETWEEN(ok_hsv360, ok_hsv48, 360, 65535, 255, 65535);
 
-#undef CHAIN_FROM
+H_SV_RATIO_BETWEEN(ok_hsv32, ok_hsv48, 65535, 65535, 255, 65535);
+
+#undef G_RB_RATIO_BETWEEN
+#undef H_SV_RATIO_BETWEEN
+#undef CHAIN_BETWEEN
+
+inline ok_srgb24 ok_srgb24_from(ok_hsv32 hsv) {
+}
+
+inline ok_hsv32 ok_hsv32_from(ok_srgb24 rgb) {
+}
+
+inline ok_srgb48 ok_srgb24_from(ok_hsv48 hsv) {
+}
+
+inline ok_hsv48 ok_hsv32_from(ok_srgb48 rgb) {
+}
+
+inline ok_srgb48 ok_srgb48_from(ok_lrgb48 lin) {
+}
+
+inline ok_lrgb48 ok_lrgb48_from(ok_srgb48 std) {
+}
